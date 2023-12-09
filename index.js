56ab51551527ffee6ba5edb4d25a1f329b47fe73
upomathasin <tahsinchowdhuryupoma@gmail.com>
2023-12-09 13:09:16 +0600

    fix problems

--- index.js
+++ index.js
@@ -17,13 +17,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = process.env.SECRET_MONGOURI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+const client = new MongoClient(uri);
 
 async function run() {
   try {
