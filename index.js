79a38a76335a0f8a814a281ebadbe239b4ba0030
upomathasin <tahsinchowdhuryupoma@gmail.com>
2023-12-09 12:35:53 +0600

    fix problems

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 5000 || process.env.PORT;
+const port = 5000;
 app.use(cors());
 app.use(express.json());
 
